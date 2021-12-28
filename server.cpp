0ad69fa43f7d04aee95098f1a67b822f7d38554b
Chenming Huang <quic_chenhuan@quicinc.com>
2021-12-28 14:30:41 +0800

    wificond: Update BandWiphyIndexMap when country code changed
    band_to_wiphy_index_map_ only update when wifi/sap enable/
    disable currently. If country code is changed, this map
    doesn't update, which leads to scan results filtering out
    due to band/channel info not updated in framework.
    
    This commit updates this map to avoid such issue
    
    CRs-Fixed: 3098930
    Change-Id: I1b3282c45f3a4d98c746a1995e0d601182f63cdd

--- server.cpp
+++ server.cpp
@@ -494,6 +494,7 @@ void Server::OnRegDomainChanged(uint32_t wiphy_index, std::string& country_code)
   } else {
     LOG(INFO) << "Regulatory domain changed to country: " << country_code
               << " on wiphy_index: " << wiphy_index;
+    UpdateBandWiphyIndexMap(wiphy_index);
     BroadcastRegDomainChanged(country_code);
   }
   LogSupportedBands(wiphy_index);
